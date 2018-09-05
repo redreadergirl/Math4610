ac3b005f76fa89be802776e1c370a3013928b536
redreadergirl <35277296+redreadergirl@users.noreply.github.com>
2018-09-04 23:41:27 -0600

    Update dmaceps.cpp

--- hw1/dmaceps.cpp
+++ hw1/dmaceps.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
 double dmaceps() {
 	double num = 1.0;
 	while (num + 1.0 != 1.0) {
 		num /= 2;
 	}
 	return num;
+}
+
+int main() {
+	cout << dmaceps();
+	return 0;
 }
