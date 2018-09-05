8a8155b907abd6089ed8b12bc7c7b8f3f6ed4bca
redreadergirl <35277296+redreadergirl@users.noreply.github.com>
2018-09-04 23:40:07 -0600

    Update maceps.cpp

--- hw1/maceps.cpp
+++ hw1/maceps.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
 float maceps() {
 	float num = 1;
-	while (num + 1.0 != 1.0) {
+	while (num + 1 != 1) {
 		num /= 2;
 	}
 	return num;
+}
+
+int main() {
+	cout << maceps();
+	return 0;
 }
